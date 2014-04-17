4a082ed7cf196f52fd011d0576d992fada7d7b92
Ted Whang <taeywhang@gmail.com>
2014-04-17 10:18:54 -0700

    Finish layout and routes

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "User pages" do
+  
+  subject { page }
+
+  describe "signup page" do
+  	before {  visit signup_path  }
+
+  	it {  should have_content('Sign up')  }
+  	it {  should have_title('Ruby on Rails Tutorial Sample App | Sign up')  }
+  end
+end
